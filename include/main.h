82d89482a6bd52c7917ed28a20792c7d6c177ccd
Filiph Sandström <filiph.sandstrom@filfatstudios.com>
2016-04-25 17:31:20 +0200

    Hide _main.

--- include/main.h
+++ include/main.h
@@ -1,10 +1,14 @@
 #pragma once
 #include "coreinit.h"
 
+void __main(int argc, char **argv);
 #ifdef __cplusplus
 extern "C" {
 #endif
-    int _main(int argc, char **argv);
+    int _main(int argc, char **argv) {
+        __main(argc, argv);
+        return 0;
+    }
     void _osscreeninit();
     void _printstr(char *str);
     void _osscreenexit();
--- source/main.cpp
+++ source/main.cpp
@@ -4,10 +4,3 @@
 void __main(int argc, char **argv) {
     OSFatal("Hello World!");
 }
-
-extern "C" {
-    int _main(int argc, char **argv) {
-        __main(argc, argv);
-        return 0;
-    }
-}
